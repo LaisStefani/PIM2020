8acac0b060222866b8870af33223e8074a0a6ee9
Lais Stefani Costa Melo <lais.melo@interfile.com.br>
2020-10-06 19:05:25 -0300

    Esse é o primeiro commit

---
+++ nome do projeto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nome_do_projeto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
